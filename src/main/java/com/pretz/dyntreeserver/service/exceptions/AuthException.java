f77a8f1419de9a951710ecd587612e0b5315c75c
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-01-20 20:54:14 +0100

    Refactored exception message concatenating

--- src/main/java/com/pretz/dyntreeserver/service/exceptions/AuthException.java
+++ src/main/java/com/pretz/dyntreeserver/service/exceptions/AuthException.java
@@ -2,10 +2,10 @@ package com.pretz.dyntreeserver.service.exceptions;
 
 public class AuthException extends RuntimeException {
     protected static String authExceptionMsg = "Incorrect user name or password for user: ";
-    protected String userName;
+    protected String exMessage;
 
     public AuthException(String userName) {
         super(authExceptionMsg + userName);
-        this.userName = userName;
+        this.exMessage = authExceptionMsg + userName;
     }
 }
