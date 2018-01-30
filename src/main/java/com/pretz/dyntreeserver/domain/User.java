776f3bd5d759e4bc2e31d35f66ba1edb09a4f5a9
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-01-30 22:20:33 +0100

    Fixed JSON password deserialization bug

--- src/main/java/com/pretz/dyntreeserver/domain/User.java
+++ src/main/java/com/pretz/dyntreeserver/domain/User.java
@@ -1,6 +1,7 @@
 package com.pretz.dyntreeserver.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 
@@ -16,7 +17,7 @@ public class User {
     private String name;
 
     @Column(name = "password")
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Column(name = "email")
@@ -53,6 +54,7 @@ public class User {
         this.name = name;
     }
 
+    @JsonIgnore
     public String getPassword() {
         return password;
     }
